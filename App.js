efa7db1098d0b5ec859d6ec8b2b94f8c8d1ae3db
Marcus B <marcus.brederfalt@systementor.se>
2024-01-25 13:19:56 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useState } from 'react';
+import Header from './components/Header';
+import AddTodo from './components/AddTodo';
+import TodoItem from "./components/TodoItem";
+
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: "Boka tvättid", key: "1"},
+    {text: "spela tv-spel", key: "2"},
+    {text: "Diska", key: "3"},
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  } 
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString() },
+        ...prevTodos
+      ]
+
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Header />
+   
+    <View style={styles.content}>
+    <AddTodo submitHandler={submitHandler}/>
+   
+    <View style={styles.list}>
+    <FlatList
+    data={todos}
+    renderItem={({item}) => (
+      <TodoItem item={item} pressHandler={pressHandler} />
+
+    )} />
+     </View>
+    </View>
     </View>
   );
 }
@@ -13,8 +52,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
+  content: {
+    padding: 40,
+  },
+list: {
+  marginTop: 20,
+},
+
+
 });
